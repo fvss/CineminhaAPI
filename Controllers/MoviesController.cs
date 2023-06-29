0cac310c99ebc19665dbdbaf23efd7493f2d0d19
Flávio Veras <flaviov.sampaio@hotmail.com>
2023-06-29 14:43:29 -0300

    Primeiro Push.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace CineminhaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ILogger<MoviesController> _logger;
+        private TempMockRepository _repository;
+
+        public MoviesController(ILogger<MoviesController> logger)
+        {
+            _logger = logger;
+            _repository = new TempMockRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return _repository.ReturnMovies();
+        }
+    }
+}
